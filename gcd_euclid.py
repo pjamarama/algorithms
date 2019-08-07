4aa3985f1d109e1af2ec56d57df631bf9bed0c71
Alexey <lukomorie2001@gmail.com>
2019-08-07 08:54:14 +0300

    naive algo corrected, new euclid realizations added

--- gcd_euclid.py
+++ gcd_euclid.py
@@ -2,13 +2,28 @@ a = int(input("Enter first nuber: "))
 b = int(input("Enter second nuber: "))
 
 def gcd_euclid(a, b):
-    if a == 0:
-        return b
-    elif b == 0:
-        return a
+    assert a > 0 and b > 0
+    if a == 0 or b == 0:
+        return max(a, b)
     elif a >= b:
-        return gcd_euclid(a%b, b)
-    elif a >= a:
-        return gcd_euclid(b%a, a)
+        return gcd_euclid(a % b, b)
+    else:
+        return gcd_euclid(b % a, a)
+
+def gcd3(a, b): # recursion, no condition statements
+    assert a > 0 and b > 0
+    if a == 0 or b == 0:
+        return max(a, b)
+    return gcd3(b % a, a)
+
+# Stepik example no recursion
+def gcd2(a, b):
+    while a and b:
+        if a >= b:  
+            a %= b # a is swapped with remainder
+        else:
+            b %= a
+    return max(a, b) # number !=0 is returned 
 
 print(f"Greatest common divisor: {gcd_euclid(a, b)}")
+
--- gcd_naive.py
+++ gcd_naive.py
@@ -2,8 +2,10 @@ a = int(input("Enter first nuber: "))
 b = int(input("Enter second nuber: "))
 
 def gcd_naive(a, b):
-    for d in range(2, max(a, b)):
-        if (a%d == 0) and (b%d == 0):
+    assert a > 0 and b > 0
+    for d in reversed(range(2, max(a, b))):
+        if a%d == b%d ==0:
+        # if (a%d == 0) and (b%d == 0):
             return d
     return "No common devisor exept 1"
 
